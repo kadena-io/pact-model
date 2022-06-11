33224744ab3aa940c0677fba128a5a1a176518f0
John Wiegley <johnw@newartisans.com>
2022-06-10 22:22:05 -0700

    Add work on semantics

--- src/Pact.v
+++ src/Pact.v
@@ -24,13 +24,23 @@ Inductive Ty :=
 
 Infix "⟶" := TFunc (at level 30, right associativity).
 
-Fixpoint tdenote (τ : Ty) : Type :=
+Fixpoint SemTy (τ : Ty) : Type :=
   match τ with
   | TUnit => unit
-  | TFunc dom cod => tdenote dom → tdenote cod
+  | TFunc dom cod => SemTy dom → SemTy cod
   end.
 
 Definition Env := list Ty.
+
+Fixpoint SemEnv E : Type :=
+  match E with
+  | nil    => unit
+  | t :: E => prodT (SemTy t) (SemEnv E)
+  end.
+
+(* Inductive SemEnv : Env → Type := *)
+(*   | ANil      : SemEnv [] *)
+(*   | ACons τ Γ : SemTy τ → SemEnv Γ → SemEnv (τ :: Γ). *)
 
 (* Notation "·" := Empty. *)
 (* Notation "Γ ⸴ A ： τ" := (@Cons τ A _ _ Γ) (at level 80, right associativity). *)
@@ -57,12 +67,19 @@ Definition Sub Γ Γ' := ∀ {τ}, Var Γ τ → Exp Γ' τ.
 
 Definition idSub {Γ} : Sub Γ Γ := @VAR Γ.
 
+(*
 Program Definition consSub {Γ Γ' τ} (e : Exp Γ' τ)
         (s : Sub Γ Γ') : Sub (τ :: Γ) Γ' :=
   λ _ v, match v with
          | ZV    => e
          | SV v' => s _ v'
          end.
+*)
+
+Equations consSub {Γ Γ' τ} (e : Exp Γ' τ) (s : Sub Γ Γ')
+          τ' (v : Var (τ :: Γ) τ') : Exp Γ' τ' :=
+  consSub e s τ' ZV      := e;
+  consSub e s τ' (SV v') := s _ v'.
 
 Notation "{| e ; .. ; f |}" := (consSub e .. (consSub f idSub) ..).
 
@@ -72,13 +89,17 @@ Definition hdSub {Γ Γ' τ} (s : Sub (τ :: Γ) Γ') : Exp Γ' τ := s τ ZV.
 
 Definition Ren Γ Γ' := ∀ {τ}, Var Γ τ → Var Γ' τ.
 
+Definition tlRen {Γ Γ' τ} (r : Ren (τ :: Γ) Γ') : Ren Γ Γ' :=
+  fun τ' v => r τ' (SV v).
+Definition hdRen {Γ Γ' τ} (r : Ren (τ :: Γ) Γ') : Var Γ' τ := r τ ZV.
+
 (*
-Program Definition RTmL {Γ Γ' τ} (r : Ren Γ Γ') : Ren (τ :: Γ) (τ :: Γ') :=
-  λ τ' v,
-    match v with
-    | ZV    => ZV
-    | SV v' => SV (r _ v')
-    end.
+Program Definition RTmL {Γ Γ' τ} (r : Ren Γ Γ') :
+        Ren (τ :: Γ) (τ :: Γ') :=
+  λ _ v, match v with
+         | ZV    => ZV
+         | SV v' => SV (r _ v')
+         end.
 *)
 
 (* This version gives better rewriting equations. *)
@@ -96,17 +117,18 @@ Proof.
   - now rewrite RTmL_equation_2.
 Qed.
 
-Fixpoint rename {Γ Γ' τ} (r : Ren Γ Γ') (e : Exp Γ τ) : Exp Γ' τ :=
+Fixpoint RTmExp {Γ Γ' τ} (r : Ren Γ Γ') (e : Exp Γ τ) : Exp Γ' τ :=
   match e with
   | VAR v     => VAR (r _ v)
-  | APP e1 e2 => APP (rename r e1) (rename r e2)
-  | ABS e     => ABS (rename (RTmL r) e)
+  | APP e1 e2 => APP (RTmExp r e1) (RTmExp r e2)
+  | ABS e     => ABS (RTmExp (RTmL r) e)
   end.
 
-Definition wk {Γ τ τ'} : Exp Γ τ → Exp (τ' :: Γ) τ := rename (λ _, SV).
+Definition wk {Γ τ τ'} : Exp Γ τ → Exp (τ' :: Γ) τ := RTmExp (λ _, SV).
 
 (*
-Program Definition STmL {Γ Γ' τ} (s : Sub Γ Γ') : Sub (τ :: Γ) (τ :: Γ') :=
+Program Definition STmL {Γ Γ' τ} (s : Sub Γ Γ') :
+        Sub (τ :: Γ) (τ :: Γ') :=
   λ _ v, match v with
          | ZV    => VAR ZV
          | SV v' => wk (s _ v')
@@ -140,131 +162,190 @@ Definition compose {Γ τ τ' τ''}
            (g : Exp Γ (τ ⟶ τ')) : Exp Γ (τ ⟶ τ'') :=
   ABS (APP (wk f) (APP (wk g) (VAR ZV))).
 
-Inductive Args (P : Ty → Type) : Env → Type :=
-  | ANil      : Args P []
-  | ACons τ Γ : P τ → Args P Γ → Args P (τ :: Γ).
+Fixpoint SemVar `(v : Var Γ τ) : SemEnv Γ → SemTy τ :=
+  match v with
+  | ZV   => λ se, fst se
+  | SV v => λ se, SemVar v (snd se)
+  end.
 
-Arguments ANil {P}.
-Arguments ACons {P _ _} _ _.
+Fixpoint SemExp `(e : Exp Γ τ) : SemEnv Γ → SemTy τ :=
+  match e with
+  | VAR v     => SemVar v
+  | ABS e     => λ se, λ x, SemExp e (x, se)
+  | APP e1 e2 => λ se, SemExp e1 se (SemExp e2 se)
+  end.
 
-Equations get (P : Ty → Type) {Γ : Env} (A : Args P Γ)
-          {τ} (v : Var Γ τ) : P τ :=
-  get P (ACons x _)  ZV      := x;
-  get P (ACons _ xs) (SV v') := get P xs v'.
+Fixpoint SemSub {Γ Γ'} : Sub Γ' Γ → SemEnv Γ → SemEnv Γ' :=
+  match Γ' with
+  | nil    => λ s se, tt
+  | _ :: _ => λ s se, (SemExp (hdSub s) se, SemSub (tlSub s) se)
+  end.
 
-Equations denote `(A : Args tdenote Γ) `(e : Exp Γ τ) : tdenote τ :=
-  denote A (VAR v)   := get tdenote A v;
-  denote A (ABS e)   := λ x, denote (ACons x A) e;
-  denote A (APP f x) := denote A f (denote A x).
+Fixpoint SemRen {Γ Γ'} : Ren Γ' Γ → SemEnv Γ → SemEnv Γ' :=
+  match Γ' with
+  | nil    => λ r se, tt
+  | _ :: _ => λ r se, (SemVar (hdRen r) se, SemRen (tlRen r) se)
+  end.
 
-Equations denoteR `(A : Args tdenote Γ') `(e : Exp Γ τ) (r : Ren Γ Γ') :
-  tdenote τ :=
-  denoteR A (VAR v)   r := get tdenote A (r _ v);
-  denoteR A (ABS e)   r := λ x, denoteR (ACons x A) e (RTmL r);
-  denoteR A (APP f x) r := denoteR A f r (denoteR A x r).
-
-Lemma denote_identity `(A : Args tdenote Γ) τ :
-  denote A (identity Γ τ) = id.
+Lemma SemRenComm Γ t (e : Exp Γ t) Γ' (r : Ren Γ Γ') :
+  ∀ se, SemExp e (SemRen r se) = SemExp (RTmExp r e) se.
 Proof.
-  extensionality x.
-  unfold identity, id.
-  rewrite denote_equation_2.
-  rewrite denote_equation_1.
-  now rewrite get_equation_2.
-Qed.
-
-Lemma denote_denoteR `(A : Args tdenote Γ) {τ} (e : Exp Γ τ) :
-  denoteR A e (λ _, id) = denote A e.
-Proof.
-  induction e.
-  - rewrite denote_equation_1.
-    now rewrite denoteR_equation_1.
-  - rewrite denote_equation_2.
-    rewrite denoteR_equation_2.
-    extensionality x.
-    fold tdenote in x.
-    rewrite RTmL_id.
-    now rewrite IHe.
-  - rewrite denote_equation_3.
-    rewrite denoteR_equation_3.
-    now rewrite IHe1, IHe2.
-Qed.
-
-Lemma denoteR_rename `(A : Args tdenote Γ') `(r : Ren Γ Γ')
-      {τ} (e : Exp Γ τ) :
-  denoteR A (rename r e) (λ _, id) = denoteR A e r.
-Proof.
-  generalize dependent A.
-  generalize dependent r.
+  intros.
   generalize dependent Γ'.
   induction e; simpl; intros.
-  - now rewrite !denoteR_equation_1.
-  - rewrite !denoteR_equation_2.
-    extensionality x0.
-    fold tdenote in x0.
-    rewrite RTmL_id.
-    now rewrite IHe.
-  - rewrite denoteR_equation_3.
-    now rewrite IHe1, IHe2.
+  - induction v; simpl.
+    + reflexivity.
+    + now rewrite IHv.
+  - extensionality x.
+    fold SemTy in x.
+    rewrite <- IHe; clear IHe.
+    simpl.
+    f_equal.
+    clear.
+    f_equal.
+    unfold tlRen.
+    induction Γ; simpl; auto.
+    f_equal.
+    rewrite IHΓ; clear IHΓ.
+    reflexivity.
+  - now rewrite IHe1, IHe2.
 Qed.
 
-Lemma denote_wk `(A : Args tdenote Γ)
-      {τ τ'} (x : tdenote τ') (e : Exp Γ τ) :
-  denote (ACons x A) (wk e) = denote A e.
+Definition RcR {Γ Γ' Γ''} (r : Ren Γ' Γ'') (r' : Ren Γ Γ') :=
+  (λ t v, r t (r' t v)) : Ren Γ Γ''.
+
+Definition ScR {Γ Γ' Γ''} (s : Sub Γ' Γ'') (r : Ren Γ Γ') :=
+  (λ t v, s t (r t v)) : Sub Γ Γ''.
+
+Definition RcS {Γ Γ' Γ''} (r : Ren Γ' Γ'') (s : Sub Γ Γ') :=
+  (λ t v, RTmExp r (s t v)) : Sub Γ Γ''.
+
+Definition ScS {Γ Γ' Γ''} (s : Sub Γ' Γ'') (s' : Sub Γ Γ') :=
+  (λ t v, STmExp s (s' t v)) : Sub Γ Γ''.
+
+Lemma LiftRcR Γ Γ' Γ'' τ (r : Ren Γ' Γ'') (r' : Ren Γ Γ') :
+  RTmL (τ := τ) (RcR r r') = RcR (RTmL r) (RTmL r').
 Proof.
-  unfold wk.
-  rewrite <- !denote_denoteR.
-  rewrite denoteR_rename.
-  generalize dependent x.
-  induction e; simpl; intros.
-  - now rewrite !denoteR_equation_1.
-  - rewrite !denoteR_equation_2.
-    extensionality x0.
-    fold tdenote in x0.
-    rewrite RTmL_id.
-    rewrite <- (IHe _ x); clear IHe.
-    admit.
-  - rewrite denoteR_equation_3.
-    now rewrite IHe1, IHe2.
+  extensionality τ'.
+  extensionality v.
+  now dependent induction v.
+Qed.
+
+Corollary tlRen_SV {Γ Γ' τ} (r : Ren (τ :: Γ) Γ') :
+  tlRen r = RcR r (λ _, SV).
+Proof. reflexivity. Qed.
+
+Lemma SemRen_SV_snd (Γ : Env) (τ : Ty) :
+  @SemRen (τ :: Γ) _ (λ _, SV) = snd.
+Proof.
+  unfold SemRen.
+  extensionality A.
+  destruct A; simpl.
+  induction Γ; destruct s0; simpl; auto.
+  f_equal.
 Admitted.
 
-Lemma denote_compose `(A : Args tdenote Γ)
-      {τ τ' τ''} (f : Exp Γ (τ' ⟶ τ'')) (g : Exp Γ (τ ⟶ τ')) :
-  denote A (compose f g) = denote A f ∘ denote A g.
+Corollary SemRen_SV `(A : SemEnv Γ) `(x : SemTy τ) :
+  SemRen (λ _, SV) (x, A) = A.
+Proof. now rewrite SemRen_SV_snd. Qed.
+
+Lemma SemExp_wk `(A : SemEnv Γ)
+      {τ τ'} (x : SemTy τ') (e : Exp Γ τ) :
+  SemExp (wk e) (x, A) = SemExp e A.
+Proof.
+  unfold wk.
+  rewrite <- SemRenComm; simpl.
+  f_equal.
+  now eapply SemRen_SV; eauto.
+Qed.
+
+Lemma SemSubComm Γ t (e : Exp Γ t) Γ' (s : Sub Γ Γ') :
+  ∀ se, SemExp e (SemSub s se) = SemExp (STmExp s e) se.
+Proof.
+  intros.
+  generalize dependent Γ'.
+  induction e; simpl; intros.
+  - induction v; simpl.
+    + reflexivity.
+    + now rewrite IHv.
+  - extensionality x.
+    fold SemTy in x.
+    rewrite <- IHe; clear IHe.
+    simpl.
+    f_equal.
+    clear.
+    f_equal.
+    unfold tlSub.
+    induction Γ; simpl; auto.
+    f_equal.
+    + unfold hdSub.
+      clear.
+      rewrite STmL_equation_2.
+      now rewrite SemExp_wk.
+    + unfold tlSub.
+      rewrite IHΓ; clear IHΓ.
+      reflexivity.
+  - now rewrite IHe1, IHe2.
+Qed.
+
+Theorem Soundness t (e : Exp nil t) v :
+  Ev e v → SemExp e = SemExp v.
+Proof.
+  intros.
+  induction H; simpl; auto.
+  extensionality se.
+  destruct se.
+  rewrite IHEv1.
+  rewrite IHEv2.
+  rewrite <- IHEv3.
+  simpl.
+  rewrite <- SemSubComm.
+  simpl.
+  unfold hdSub.
+  rewrite consSub_equation_1.
+  reflexivity.
+Qed.
+
+Lemma SemExp_identity `(A : SemEnv Γ) τ :
+  SemExp (identity Γ τ) A = id.
 Proof.
   extensionality x.
-  fold tdenote in x.
-  unfold compose.
-  rewrite denote_equation_2.
-  rewrite !denote_equation_3.
-  rewrite denote_equation_1.
-  rewrite get_equation_2.
-  now rewrite !denote_wk.
+  reflexivity.
 Qed.
 
-Lemma compose_left_identity `(A : Args tdenote Γ)
-      {τ τ'} (f : Exp Γ (τ ⟶ τ')) :
-  denote A (compose f (identity Γ τ)) = denote A f.
+Lemma SemExp_compose `(A : SemEnv Γ)
+      {τ τ' τ''} (f : Exp Γ (τ' ⟶ τ'')) (g : Exp Γ (τ ⟶ τ')) :
+  SemExp (compose f g) A = SemExp f A ∘ SemExp g A.
 Proof.
-  rewrite denote_compose.
-  now rewrite denote_identity.
+  extensionality x.
+  fold SemTy in x.
+  unfold compose; simpl.
+  now rewrite !SemExp_wk.
 Qed.
 
-Lemma compose_right_identity `(A : Args tdenote Γ)
+Lemma compose_left_identity `(A : SemEnv Γ)
       {τ τ'} (f : Exp Γ (τ ⟶ τ')) :
-  denote A (compose (identity Γ τ') f) = denote A f.
+  SemExp (compose f (identity Γ τ)) A = SemExp f A.
 Proof.
-  rewrite denote_compose.
-  now rewrite denote_identity.
+  rewrite SemExp_compose.
+  reflexivity.
 Qed.
 
-Lemma compose_assoc `(A : Args tdenote Γ)
+Lemma compose_right_identity `(A : SemEnv Γ)
+      {τ τ'} (f : Exp Γ (τ ⟶ τ')) :
+  SemExp (compose (identity Γ τ') f) A = SemExp f A.
+Proof.
+  rewrite SemExp_compose.
+  reflexivity.
+Qed.
+
+Lemma compose_assoc `(A : SemEnv Γ)
       {τ τ' τ'' τ'''}
       (f : Exp Γ (τ'' ⟶ τ'''))
       (g : Exp Γ (τ' ⟶ τ''))
       (h : Exp Γ (τ ⟶ τ')) :
-  denote A (compose f (compose g h)) = denote A (compose (compose f g) h).
+  SemExp (compose f (compose g h)) A = SemExp (compose (compose f g) h) A.
 Proof.
-  rewrite !denote_compose.
+  rewrite !SemExp_compose.
   now rewrite compose_assoc.
 Qed.

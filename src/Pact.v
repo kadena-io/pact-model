15c0420873493cf8c088380a0f1bf41bd9217c25
John Wiegley <johnw@newartisans.com>
2022-07-16 10:18:53 -0700

    Drop the Value type

--- src/Pact.v
+++ src/Pact.v
@@ -48,69 +48,6 @@ Inductive Ty : Set :=
 
 Derive NoConfusion NoConfusionHom Subterm EqDec for Ty.
 
-Inductive Value : Ty → Type :=
-  | VUnit       : Value TUnit
-  | VPair {a b} : Value a → Value b → Value (TPair a b)
-  | VString     : string → Value TString
-  | VInteger    : Z → Value TInteger
-  | VDecimal    : nat → Value TDecimal
-  | VBool       : bool → Value TBool
-  | VTime       : nat → Value TTime
-  | VList {ty}  : list (Value ty) → Value (TList ty).
-
-Derive Signature NoConfusion NoConfusionHom Subterm for Value.
-
-(* This couldn't be derived due to the recursion at [VList]. *)
-Equations Value_EqDec {t} : EqDec (Value t) :=
-  Value_EqDec (t:=TUnit)       VUnit         VUnit         := left _;
-  Value_EqDec (t:=TPair t1 t2) (VPair x1 y1) (VPair x2 y2)
-    with @eq_dec _ (Value_EqDec (t:=t1)) x1 x2 := {
-      | left  _ with @eq_dec _ (Value_EqDec (t:=t2)) y1 y2 := {
-        | left _  => left _
-        | right _ => right _
-      }
-      | right _ => right _
-  };
-  Value_EqDec (t:=TString) (VString s1) (VString s2)
-    with eq_dec s1 s2 := {
-      | left _  => left _
-      | right _ => right _
-    };
-  Value_EqDec (t:=TInteger) (VInteger i1) (VInteger i2)
-    with eq_dec i1 i2 := {
-      | left _  => left _
-      | right _ => right _
-    };
-  Value_EqDec (t:=TDecimal) (VDecimal d1) (VDecimal d2)
-    with eq_dec d1 d2 := {
-      | left _  => left _
-      | right _ => right _
-    };
-  Value_EqDec (t:=TBool) (VBool b1) (VBool b2)
-    with eq_dec b1 b2 := {
-      | left _  => left _
-      | right _ => right _
-    };
-  Value_EqDec (t:=TTime) (VTime t1) (VTime t2)
-    with eq_dec t1 t2 := {
-      | left _  => left _
-      | right _ => right _
-    };
-  Value_EqDec (t:=TList _)   (VList []) (VList []) := left _;
-  Value_EqDec (t:=TList _)   (VList (_ :: _)) (VList []) := right _;
-  Value_EqDec (t:=TList _)   (VList []) (VList (_ :: _)) := right _;
-  Value_EqDec (t:=TList ty)  (VList (x1 :: xs1)) (VList (y1 :: ys1))
-    with @eq_dec _ (Value_EqDec (t:=ty)) x1 y1 := {
-      | left _  with @eq_dec _ (list_eqdec (Value_EqDec (t:=ty))) xs1 ys1 := {
-        | left _  => left _
-        | right _ => right _
-      }
-      | right _ => right _
-    }.
-
-#[export]
-Instance Value_EqDec' {t} : EqDec (Value t) := Value_EqDec (t:=t).
-
 Record CapSig (n : string) : Set := {
   paramTy : Ty;               (* this could be a product (i.e. pair) *)
   valueTy : Ty                (* is TUnit for unmanaged caps *)
@@ -121,8 +58,73 @@ Derive NoConfusion NoConfusionHom Subterm EqDec for CapSig.
 Arguments paramTy {n} _.
 Arguments valueTy {n} _.
 
+Fixpoint denoteTy (t : Ty) : Set :=
+  match t with
+  | TUnit     => unit
+  | TPair x y => denoteTy x * denoteTy y
+  | TString   => string
+  | TInteger  => Z
+  | TDecimal  => nat
+  | TBool     => bool
+  | TTime     => nat
+  | TList t   => list (denoteTy t)
+  end.
+
+Notation "⟦ t ⟧" := (denoteTy t) (at level 9).
+
+(* This couldn't be derived due to the recursion at [VList]. *)
+Equations Value_EqDec t : EqDec ⟦t⟧ :=
+  Value_EqDec TUnit tt tt := left _;
+  Value_EqDec (TPair t1 t2) (x1, y1) (x2, y2)
+    with @eq_dec _ (Value_EqDec t1) x1 x2 := {
+      | left  _ with @eq_dec _ (Value_EqDec (t2)) y1 y2 := {
+        | left _  => left _
+        | right _ => right _
+      }
+      | right _ => right _
+  };
+  Value_EqDec TString s1 s2
+    with eq_dec s1 s2 := {
+      | left _  => left _
+      | right _ => right _
+    };
+  Value_EqDec TInteger i1 i2
+    with eq_dec i1 i2 := {
+      | left _  => left _
+      | right _ => right _
+    };
+  Value_EqDec TDecimal d1 d2
+    with eq_dec d1 d2 := {
+      | left _  => left _
+      | right _ => right _
+    };
+  Value_EqDec TBool b1 b2
+    with eq_dec b1 b2 := {
+      | left _  => left _
+      | right _ => right _
+    };
+  Value_EqDec TTime t1 t2
+    with eq_dec t1 t2 := {
+      | left _  => left _
+      | right _ => right _
+    };
+  Value_EqDec (TList _)   ([]) ([]) := left _;
+  Value_EqDec (TList _)   ((_ :: _)) ([]) := right _;
+  Value_EqDec (TList _)   ([]) ((_ :: _)) := right _;
+  Value_EqDec (TList ty)  ((x1 :: xs1)) ((y1 :: ys1))
+    with @eq_dec _ (Value_EqDec ty) x1 y1 := {
+      | left _  with @eq_dec _ (list_eqdec (Value_EqDec ty)) xs1 ys1 := {
+        | left _  => left _
+        | right _ => right _
+      }
+      | right _ => right _
+    }.
+
+#[export]
+Instance Value_EqDec' {t} : EqDec ⟦t⟧ := Value_EqDec t.
+
 Inductive Cap `(S : CapSig n) : Set :=
-  | Token : Value (paramTy S) → Value (valueTy S) → Cap S.
+  | Token : ⟦paramTy S⟧ → ⟦valueTy S⟧ → Cap S.
 
 Derive NoConfusion NoConfusionHom Subterm for Cap.
 
@@ -133,8 +135,8 @@ Arguments Token {n S} _.
 Program Instance Cap_EqDec `(S : CapSig n) : EqDec (Cap S).
 Next Obligation.
   destruct x, y, S; simpl in *.
-  destruct (@eq_dec _ (Value_EqDec (t:=paramTy0)) v v1); subst.
-  - destruct (@eq_dec _ (Value_EqDec (t:=valueTy0)) v0 v2); subst.
+  destruct (@eq_dec _ (Value_EqDec (paramTy0)) d d1); subst.
+  - destruct (@eq_dec _ (Value_EqDec (valueTy0)) d0 d2); subst.
     + now left.
     + right; congruence.
   - right; congruence.
@@ -150,8 +152,8 @@ Derive NoConfusion NoConfusionHom Subterm EqDec for ACap.
 
 Inductive CapError : Set :=
   | CapErr_UnknownCapability      : string → CapError
-  | CapErr_InvalidParameter {t}   : Value t → CapError
-  | CapErr_InvalidValue {t}       : Value t → CapError
+  | CapErr_InvalidParameter {t}   : ⟦t⟧ → CapError
+  | CapErr_InvalidValue {t}       : ⟦t⟧ → CapError
   | CapErr_CapabilityNotAvailable : ACap → CapError
   | CapErr_ManagedError           : string → CapError
   | CapErr_NoResourceAvailable    : ACap → CapError
@@ -177,7 +179,7 @@ Record PactState := {
   (* We cannot refer to capability tokens by their type here, because
      capability predicates execute in a state monad that reference this
      record type. *)
-  resources : ∀ c : ACap, option (Value (valueTy (sig c)))
+  resources : ∀ c : ACap, option ⟦valueTy (sig c)⟧
 }.
 
 Derive NoConfusion NoConfusionHom Subterm for PactState.
@@ -193,9 +195,9 @@ Definition PactM := @RWSE PactEnv PactState PactLog Err.
  *)
 
 Record DefCap `(C : ACap) : Type := {
-  predicate : Value (paramTy (sig C)) → PactM (list ACap);
-  manager   : Value (valueTy (sig C)) → Value (valueTy (sig C)) →
-              PactM (Value (valueTy (sig C)))
+  predicate : ⟦paramTy (sig C)⟧ → PactM (list ACap);
+  manager   : ⟦valueTy (sig C)⟧ → ⟦valueTy (sig C)⟧ →
+              PactM ⟦valueTy (sig C)⟧
 }.
 
 Derive NoConfusion NoConfusionHom Subterm for DefCap.

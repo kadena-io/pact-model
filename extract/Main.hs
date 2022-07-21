2f6ed6fc5bbb21505010674b4fcd8bcb3918d2ed
John Wiegley <johnw@newartisans.com>
2022-07-21 11:49:46 -0700

    Corrections to the smoke tests

--- extract/Main.hs
+++ extract/Main.hs
@@ -1,24 +1,19 @@
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Main where
 
 import Pact
 import Test.Hspec
--- import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
     describe "Basic tests" $ do
         it "Unit smoke test" $ do
-            print $ eval (TyPrim PrimUnit) (Lit PrimUnit LitUnit)
+            print $ eval (TyPrim PrimUnit) (Lit p LitUnit)
 
         it "Lambda AddInt smoke test" $ do
-            print $ eval
-              (TyPrim PrimInteger)
+            print $ eval (TyPrim PrimInteger)
               (APP t t
                    (APP t t
-                        (Bltn TySym AddInt)
+                        (Bltn t AddInt)
                         (Lit p (LitInteger 123)))
                    (Lit p (LitInteger 456)))
 

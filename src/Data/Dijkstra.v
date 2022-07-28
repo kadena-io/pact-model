19880683c50a446568de5a20a4456c3ad501332b
John Wiegley <johnw@newartisans.com>
2022-07-27 22:00:41 -0700

    More work in Dijkstra.v

--- src/Data/Dijkstra.v
+++ src/Data/Dijkstra.v
@@ -1,6 +1,9 @@
+Require Import Coq.Arith.Arith.
 Require Import Pact.Lib.
 Require Import Hask.Control.Monad.
 Require Import Hask.Control.Monad.Cont.
+Require Import Hask.Control.Monad.State.
+Require Import Hask.Control.Monad.Trans.Class.
 Require Import Hask.Control.Monad.Trans.State.
 Require Import Hask.Control.Monad.Trans.Either.
 Require Import Hask.Data.Functor.Identity.
@@ -80,12 +83,192 @@ Program Instance Wpure_Order : Order Wpure := {|
  * Example
  *)
 
+Ltac calculate :=
+  cbv [
+    Cont
+    Cont_Applicative
+    Cont_Functor
+    Cont_Monad
+    Cont_join
+    Cont_map
+    Datatypes.id
+    EitherT_Applicative
+    EitherT_Functor
+    EitherT_Monad
+    EitherT_MonadTrans
+    EitherT_join
+    EitherT_pure
+    EitherT_map
+    Either_Applicative
+    Either_Functor
+    Either_Monad
+    Either_join
+    Either_map
+    Hask.Data.Either.Either_Functor
+    Hask.Data.Either.Either_Applicative
+    Hask.Data.Either.Either_Monad
+    Hask.Data.Either.Either_map
+    Hask.Data.Either.Either_join
+    Identity_Applicative
+    Identity_Functor
+    Identity_Monad
+    Prelude.apply
+    Prelude.compose
+    Prelude.flip
+    State.get
+    State.modify
+    State.put
+    StateT_Applicative
+    StateT_Functor
+    StateT_Monad
+    StateT_MonadTrans
+    StateT_join
+    State_Applicative
+    State_Functor
+    State_Monad
+    Tuple.curry
+    Tuple.first
+    lift
+    bind
+    comp
+    fmap
+    getT
+    is_applicative
+    is_functor
+    join
+    modifyT
+    pure
+    putT
+  ] in *.
+
+Module DijkstraSt.
+
 Section DijkstraExample.
 
 Variable s : Type.
 
-Definition M := StateT s id.
+Definition M := State s.
 Definition W := StateT s (Cont Prop).
+
+#[export]
+Program Instance id_Functor : Functor id := {|
+  fmap := λ _ _ f x, f x
+|}.
+#[export]
+Program Instance id_Applicative : Applicative id := {|
+  pure := λ _ x, x;
+  ap := λ _ _ f x, f x
+|}.
+#[export]
+Program Instance id_Monad : Monad id.
+
+#[export]
+Instance M_Functor : Functor M := State_Functor.
+#[export]
+Instance M_Applicative : Applicative M := State_Applicative.
+#[export]
+Instance M_Monad : Monad M := State_Monad.
+
+#[export]
+Program Instance W_Order : Order W := {|
+  wle := λ _ w₁ w₂, ∀ p s, w₂ p s → w₁ p s
+|}.
+
+Definition θ : observation M W := λ _ c s p, p (c s).
+
+#[export]
+Program Instance θ_LaxMorphism : LaxMorphism θ.
+Next Obligation. sauto. Qed.
+
+#[export]
+Program Instance θ_Morphism : Morphism θ.
+Next Obligation.
+  unfold θ in *.
+  calculate; sauto.
+Qed.
+
+Definition ST (A : Type) (w : W A) := ∑ c : M A, θ _ c ≤ᵂ w.
+
+#[export]
+Program Instance ST_Dijkstra : DijkstraMonad ST := {|
+  retᴰ := λ _ x, (pure[M] x; _);
+  bindᴰ := λ A B w wf (c : ST A w) (f : ∀ x, ST B (wf x)),
+    (bind (m:=M) (λ x, projT1 (f x)) (projT1 c); _);
+  subcompᴰ := λ A w w' (c : ST A w) (h : w ≤ᵂ w'), _
+|}.
+Next Obligation.
+  unfold θ in *.
+  calculate.
+  destruct c; simpl in *.
+  apply w0 in H.
+  unfold θ in *.
+  destruct (x p).
+  destruct (f a);
+  sauto.
+Defined.
+Next Obligation.
+  sauto.
+Defined.
+Next Obligation.
+  unfold θ in *.
+  calculate.
+  extensionality s0;
+  sauto.
+Defined.
+
+Program Definition liftST0 `(f : M a) : ST a (θ _ f) := (f; _).
+Program Definition liftST1 `(f : a → M b) (x : a) :
+  ST b (θ _ (f x)) := (f x; _).
+
+Definition getST := liftST0 State.get.
+Definition putST := liftST1 State.put.
+
+Definition modify' := liftST1 State.modify.
+
+Definition modifyST `(f : s → s) := bindᴰ getST (λ x, putST (f x)).
+
+End DijkstraExample.
+
+Goal True.
+  pose getST.
+  pose putST.
+  pose modify'.
+  pose modifyST.
+  unfold θ in *.
+  calculate.
+
+  auto.
+Qed.
+
+Definition algorithm : M nat nat :=
+  n <- getT ;
+  if n <? 10
+  then
+    putT (n + 1) ;;
+    pure (n + 100)
+  else pure 0.
+
+Definition algorithmST := liftST0 _ algorithm.
+
+Goal True.
+  pose algorithmST.
+  unfold θ, algorithm in s.
+  calculate.
+
+  auto.
+Qed.
+
+End DijkstraSt.
+
+Module DijkstraSte.
+
+Section DijkstraExample.
+
+Variable s : Type.
+Variable e : Type.
+
+Definition M := StateT s (Either e).
+Definition W := StateT s (EitherT e (Cont Prop)).
 
 #[export]
 Program Instance id_Functor : Functor id := {|
@@ -111,135 +294,210 @@ Program Instance W_Order : Order W := {|
   wle := λ _ w₁ w₂, ∀ p s, w₂ p s → w₁ p s
 |}.
 
-Definition θst : observation M W := λ _ c s p, p (c s).
+Definition θbot : observation M W := λ _ c s p,
+  match c s with
+  | inl _ => False
+  | inr x => p (inr x)
+  end.
 
 #[export]
-Program Instance θst_LaxMorphism : LaxMorphism θst.
+Program Instance θbot_LaxMorphism : LaxMorphism θbot.
 Next Obligation. sauto. Qed.
+Next Obligation.
+  unfold θbot in *.
+  calculate; sauto.
+Qed.
 
 #[export]
-Program Instance θst_Morphism : Morphism θst.
+Program Instance θbot_Morphism : Morphism θbot.
 Next Obligation.
-  unfold Cont_map, Prelude.flip, Prelude.compose,
-    comp, Tuple.first, θst in *.
+  unfold θbot in *.
+  calculate.
+  extensionality s0.
+  extensionality p.
   sauto.
 Qed.
 
-Definition ST (A : Type) (w : W A) := ∑ c : M A, θst _ c ≤ᵂ w.
+Definition θtop : observation M W := λ _ c s p,
+  match c s with
+  | inl _ => True
+  | inr x => p (inr x)
+  end.
 
 #[export]
-Program Instance ST_Dijkstra : DijkstraMonad ST := {|
+Program Instance θtop_LaxMorphism : LaxMorphism θtop.
+Next Obligation.
+  unfold θtop in *.
+  calculate; sauto.
+Qed.
+
+#[export]
+Program Instance θtop_Morphism : Morphism θtop.
+Next Obligation.
+  unfold θtop in *.
+  calculate.
+  extensionality s0.
+  extensionality p.
+  sauto.
+Qed.
+
+Definition STbot (A : Type) (w : W A) := ∑ c : M A, θbot _ c ≤ᵂ w.
+Definition STtop (A : Type) (w : W A) := ∑ c : M A, θtop _ c ≤ᵂ w.
+
+#[export]
+Program Instance STbot_Dijkstra : DijkstraMonad STbot := {|
   retᴰ := λ _ x, (pure[M] x; _);
-  bindᴰ := λ A B w wf (c : ST A w) (f : ∀ x, ST B (wf x)),
+  bindᴰ := λ A B w wf (c : STbot A w) (f : ∀ x, STbot B (wf x)),
     (bind (m:=M) (λ x, projT1 (f x)) (projT1 c); _);
-  subcompᴰ := λ A w w' (c : ST A w) (h : w ≤ᵂ w'), _
+  subcompᴰ := λ A w w' (c : STbot A w) (h : w ≤ᵂ w'), _
 |}.
 Next Obligation.
-  unfold Cont_join, Cont_map, Prelude.flip, Prelude.compose,
-    comp, Tuple.first, θst, Datatypes.id, StateT_join, comp,
-    curry, Prelude.apply in *.
+  unfold θbot in *.
   simpl in *.
   unfold Datatypes.id.
   destruct c; simpl in *.
   apply w0 in H.
-  unfold θst in *.
+  unfold θbot in *.
   destruct (x p).
-  destruct (f a); simpl.
-  sauto.
+  - calculate; sauto.
+  - calculate.
+    destruct p0.
+    destruct (f a).
+    unfold θbot in *.
+    sauto.
 Defined.
 Next Obligation.
   sauto.
 Defined.
+
+#[export]
+Program Instance STtop_Dijkstra : DijkstraMonad STtop := {|
+  retᴰ := λ _ x, (pure[M] x; _);
+  bindᴰ := λ A B w wf (c : STtop A w) (f : ∀ x, STtop B (wf x)),
+    (bind (m:=M) (λ x, projT1 (f x)) (projT1 c); _);
+  subcompᴰ := λ A w w' (c : STtop A w) (h : w ≤ᵂ w'), _
+|}.
 Next Obligation.
-  unfold Cont_join, Cont_map, Prelude.flip, Prelude.compose,
-    comp, Tuple.first, θst, Datatypes.id, StateT_join, comp,
-    curry, Prelude.apply in *.
-  extensionality s0; simpl.
+  unfold θtop in *.
+  simpl in *.
+  unfold Datatypes.id.
+  destruct c; simpl in *.
+  apply w0 in H.
+  unfold θtop in *.
+  destruct (x p).
+  - calculate; sauto.
+  - calculate.
+    destruct p0.
+    destruct (f a).
+    unfold θtop in *.
+    sauto.
+Defined.
+Next Obligation.
   sauto.
 Defined.
 
-Program Definition liftST0 `(f : M a) : ST a (θst _ f) := (f; _).
-Program Definition liftST1 `(f : a → M b) (x : a) :
-  ST b (θst _ (f x)) := (f x; _).
+Program Definition liftSTbot0 `(f : M a) : STbot a (θbot _ f) := (f; _).
+Program Definition liftSTbot1 `(f : a → M b) (x : a) :
+  STbot b (θbot _ (f x)) := (f x; _).
 
-Definition getST := liftST0 State.get.
-Definition putST := liftST1 State.put.
+Program Definition liftSTtop0 `(f : M a) : STtop a (θtop _ f) := (f; _).
+Program Definition liftSTtop1 `(f : a → M b) (x : a) :
+  STtop b (θtop _ (f x)) := (f x; _).
 
-Definition modify' := liftST1 State.modify.
+Definition getSTbot    := liftSTbot0 State.getT.
+Definition putSTbot    := liftSTbot1 State.putT.
+Definition modifySTbot := liftSTbot1 State.modifyT.
 
-Definition modifyST `(f : s → s) := bindᴰ getST (λ x, putST (f x)).
+Definition getSTtop    := liftSTtop0 State.getT.
+Definition putSTtop    := liftSTtop1 State.putT.
+Definition modifySTtop := liftSTtop1 State.modifyT.
 
 End DijkstraExample.
 
-Ltac calculate H :=
-  cbv [
-    Cont
-    Cont_Applicative
-    Cont_Functor
-    Cont_Monad
-    Cont_join
-    Cont_map
-    Datatypes.id
-    Identity_Applicative
-    Identity_Functor
-    Identity_Monad
-    Prelude.apply
-    Prelude.compose
-    Prelude.flip
-    State.get
-    State.modify
-    State.put
-    StateT_Applicative
-    StateT_Functor
-    StateT_Monad
-    StateT_join
-    Tuple.curry
-    Tuple.first
-    bind
-    comp
-    fmap
-    getT
-    is_applicative
-    is_functor
-    join
-    modifyT
-    pure
-    putT
-    θst
-  ] in H.
-
 Goal True.
-  pose getST.
-  calculate s.
-
-  pose putST.
-  calculate s0.
-
-  pose modify'.
-  calculate s1.
-
-  pose modifyST.
-  calculate s2.
+  pose getSTbot.
+  pose putSTbot.
+  pose modifySTbot.
+  pose getSTtop.
+  pose putSTtop.
+  pose modifySTtop.
+  unfold θbot, θtop in *.
+  calculate.
 
   auto.
 Qed.
 
-Require Import Coq.Arith.Arith.
-
-Definition algorithm : M nat nat :=
-  n <- getT ;
-  if n <? 10
+Definition assert {s e : Type} (b : bool) (err : e) : M s e unit :=
+  if b
   then
-    putT (n + 1) ;;
-    pure (n + 100)
-  else pure 0.
+    pure tt
+  else
+    lift (inl err).
 
-Definition algorithmST := liftST0 _ algorithm.
+Arguments assert _ _ /.
+
+Definition algorithm (x : nat) : M nat unit nat :=
+  n <- getT ;
+  assert (n <? 10) tt ;;
+  putT (n + x) ;;
+  assert (x =? 100) tt ;;
+  r <- getT ;
+  assert (Nat.even r) tt ;;
+  pure (n + 100).
+
+Definition algorithmST := liftSTbot1 _ _ algorithm.
+
+Lemma if_fun {A B : Type} b (P Q : A → B) (x : A) :
+  (if b then λ x, P x else λ x, Q x) x = if b then P x else Q x.
+Proof. sauto. Qed.
+
+Lemma match_if {A B C : Type} b (P Q : A + B) (F : A → C) (G : B → C) :
+  match (if b then P else Q) with
+  | inl x => F x
+  | inr x => G x
+  end =
+  if b
+  then
+    match P with
+    | inl x => F x
+    | inr x => G x
+    end
+  else
+    match Q with
+    | inl x => F x
+    | inr x => G x
+    end.
+Proof. sauto. Qed.
+
+Lemma in_ST {A B C : Type} {P Q : W A B C} :
+  forall H : P = Q,
+  STbot A B C P = STbot A B C Q.
+Proof. sauto. Qed.
+
+Lemma in_STx {A B C D : Type} {P Q : D → W A B C} :
+  forall H : P = Q,
+  (∀ x : D, STbot A B C (P x)) = (∀ x : D, STbot A B C (Q x)).
+Proof. sauto. Qed.
+
+Ltac calc :=
+  repeat
+    (calculate;
+     cbv [ θbot algorithm assert M_Functor M_Applicative M_Monad ] in *
+    ).
 
 Goal True.
   pose algorithmST.
-  unfold algorithm in s.
-  calculate s.
+  calc.
+  unshelve erewrite (in_STx _) in s. {
+    extensionality s0.
+    extensionality p.
+    extensionality x.
+    repeat rewrite !if_fun !match_if.
+    reflexivity.
+  }
+  simpl in s.
 
   auto.
 Qed.
+
+End DijkstraSte.
